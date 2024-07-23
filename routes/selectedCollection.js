cc94cf969703ddd16cd5099c4b98c75b1b87f593
shubhamjoshi2023 <shubham.joshi@goapptiv.com>
2024-07-24 00:19:20 +0530

    update add collection

--- routes/selectedCollection.js
+++ routes/selectedCollection.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const Collection = require("../models/collection");
-
+const SelectedCollection = require("../models/selectedCollection");
 let selectedCollectionId = null;
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   selectedCollectionId = req.body.id;
-  res.send({ message: "Selected collection updated" });
+
+  try {
+    const newSelectedCollection = new SelectedCollection({
+      collectionId: selectedCollectionId,
+    });
+    await newSelectedCollection.save();
+    res.send({ message: "Selected collection updated and saved to database" });
+  } catch (error) {
+    res.status(500).send({ error: "Error saving selected collection" });
+  }
 });
 
 router.get("/", async (req, res) => {
+  console.log(selectedCollectionId);
   if (selectedCollectionId) {
     const selectedCollection = await Collection.findById(selectedCollectionId);
+    console.log(selectedCollection);
     if (!selectedCollection) {
       return res.status(404).send();
     }
